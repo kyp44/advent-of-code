d54232ff232279f108f25765c4f250eb81497ae1
Dan Whitman <dwhitman44@mgail.com>
2021-07-25 20:39:36 -0400

    Transfer. Started working on the 2020 day problem.

--- src/aoc_2020/day_20.rs
+++ src/aoc_2020/day_20.rs
@@ -1,4 +1,15 @@
-use crate::aoc::Solution;
+use std::{collections::HashMap, str::FromStr};
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, line_ending, one_of},
+    combinator::map,
+    multi::{many1, separated_list1},
+    sequence::{delimited, pair},
+    Finish,
+};
+
+use crate::aoc::{AocError, DiscardInput, ParseError, ParseResult, Parseable, Solution};
 
 #[cfg(test)]
 mod tests {
@@ -115,6 +126,62 @@ Tile 3079:
 ..#.......
 ..#.###...",
     vec![]
+    }
+}
+
+enum Edge {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+enum Rotation {
+    Deg0,
+    Deg90,
+    Deg180,
+    Deg270,
+}
+
+struct Tile {
+    id: u64,
+    edges: HashMap<Edge, Vec<bool>>,
+    edges_reversed: HashMap<Edge, Vec<bool>>,
+}
+impl FromStr for Tile {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (id, lv) = map::<_, _, _, ParseError, _, _>(
+            pair(
+                delimited(tag("Tile "), digit1, pair(tag(":"), line_ending)),
+                separated_list1(line_ending, many1(one_of(".#"))),
+            ),
+            |(ids, lv): (&str, Vec<Vec<char>>)| (ids.parse().unwrap(), lv),
+        )(s)
+        .finish()
+        .discard_input()?;
+
+        // Verify the tile dimensions
+        let size = lv[0].len();
+        if lv.len() != size {
+            return Err(AocError::InvalidInput(format!(
+                "Tile {} did not have the expected height of {}",
+                id, size
+            )));
+        }
+        if lv.iter().any(|v| v.len() != size) {
+            return Err(AocError::InvalidInput(format!(
+                "Not all rows of tile {} have the expected width of {}",
+                id, size
+            )));
+        }
+
+        Ok(Tile {
+            id,
+            edges: HashMap::new(),
+            edges_reversed: HashMap::new(),
+        })
     }
 }
 
