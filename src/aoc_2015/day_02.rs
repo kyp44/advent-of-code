f084f21bf0f56ca66345b78f644d7e300ae757f5
Dan Whitman <dwhitman44@gmail.com>
2021-08-15 23:28:44 -0400

    Completed the 2015 day 2 problem.

---
+++ src/aoc_2015/day_02.rs
@@ -0,0 +1,118 @@
+use std::str::FromStr;
+
+use itertools::Itertools;
+use nom::{
+    bytes::complete::tag, character::complete::digit1, combinator::map, multi::separated_list1,
+    Finish,
+};
+
+use crate::aoc::prelude::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::solution_test;
+    use Answer::Unsigned;
+
+    solution_test! {
+        vec![Unsigned(1588178), Unsigned(3783758)],
+        "2x3x4",
+    vec![58u64, 34].answer_vec(),
+    "1x1x10",
+    vec![43u64, 14].answer_vec()
+    }
+}
+
+struct Present {
+    // Will always be sorted from least to greatest
+    dimensions: Vec<u64>,
+}
+impl FromStr for Present {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut dimensions = separated_list1::<_, _, _, NomParseError, _, _>(
+            tag("x"),
+            map(digit1, |ds: &str| -> u64 { ds.parse().unwrap() }),
+        )(s)
+        .finish()
+        .discard_input()?;
+
+        if dimensions.len() != 3 {
+            return Err(AocError::InvalidInput(
+                format!(
+                    "Present {} has {} dimensions when 3 are expected",
+                    s,
+                    dimensions.len()
+                )
+                .into(),
+            ));
+        }
+        dimensions.sort_unstable();
+
+        Ok(Present { dimensions })
+    }
+}
+impl Present {
+    fn needed_paper(&self) -> u64 {
+        self.dimensions
+            .iter()
+            .combinations(2)
+            .map(|cv| 2 * cv.into_iter().product::<u64>())
+            .sum::<u64>()
+            + self.dimensions[0] * self.dimensions[1]
+    }
+
+    fn needed_ribbon(&self) -> u64 {
+        self.dimensions[..2].iter().map(|d| 2 * *d).sum::<u64>()
+            + self.dimensions.iter().product::<u64>()
+    }
+}
+
+struct Presents {
+    presents: Vec<Present>,
+}
+impl FromStr for Presents {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Presents {
+            presents: s
+                .lines()
+                .map(|line| line.parse())
+                .collect::<Result<_, _>>()?,
+        })
+    }
+}
+impl Presents {
+    fn needed_paper(&self) -> u64 {
+        self.presents.iter().map(|p| p.needed_paper()).sum()
+    }
+
+    fn needed_ribbon(&self) -> u64 {
+        self.presents.iter().map(|p| p.needed_ribbon()).sum()
+    }
+}
+
+pub const SOLUTION: Solution = Solution {
+    day: 2,
+    name: "I Was Told There Would Be No Math",
+    solvers: &[
+        // Part a)
+        |input| {
+            // Generation
+            let presents: Presents = input.parse()?;
+
+            // Process
+            Ok(presents.needed_paper().into())
+        },
+        // Part b)
+        |input| {
+            // Generation
+            let presents: Presents = input.parse()?;
+
+            // Process
+            Ok(presents.needed_ribbon().into())
+        },
+    ],
+};
