cc708283dfb2657aae9f02c945cce945eaf1094e
Dan Whitman <daniel.whitman.2@us.af.mil>
2021-09-01 11:20:22 -0400

    Completed the 2015 day 16 problem!

--- src/aoc_2015/day_16.rs
+++ src/aoc_2015/day_16.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+
+use maplit::hashmap;
+use nom::{
+    bytes::complete::{tag, take_until},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+};
+
 use crate::aoc::prelude::*;
 
 #[cfg(test)]
@@ -7,22 +17,120 @@ mod tests {
     use Answer::Unsigned;
 
     solution_test! {
-    vec![Unsigned(0)],
+    vec![Unsigned(40), Unsigned(241)],
     "",
-    vec![0u64].answer_vec()
+    Vec::new()
     }
+}
+
+#[derive(Debug)]
+struct Sue<'a> {
+    number: u16,
+    compounds: HashMap<&'a str, u8>,
+}
+impl<'a> Parseable<'a> for Sue<'a> {
+    fn parser(input: &'a str) -> NomParseResult<Self> {
+        map(
+            separated_pair(
+                preceded(tag("Sue "), nom::character::complete::u16),
+                tag(": "),
+                separated_list1(
+                    tag(", "),
+                    separated_pair(take_until(":"), tag(": "), nom::character::complete::u8),
+                ),
+            ),
+            |(number, cs)| Sue {
+                number,
+                compounds: cs.into_iter().collect(),
+            },
+        )(input.trim())
+    }
+}
+impl<'a> Sue<'a> {
+    fn new(compounds: HashMap<&'a str, u8>) -> Self {
+        Sue {
+            number: 0,
+            compounds,
+        }
+    }
+}
+
+trait Part {
+    fn matches(a: &Sue, b: &Sue) -> bool;
+}
+struct PartA;
+impl Part for PartA {
+    fn matches(a: &Sue, b: &Sue) -> bool {
+        a.compounds
+            .keys()
+            .filter(|k| b.compounds.contains_key(*k))
+            .all(|k| a.compounds.get(k).unwrap() == b.compounds.get(k).unwrap())
+    }
+}
+struct PartB;
+impl Part for PartB {
+    fn matches(a: &Sue, b: &Sue) -> bool {
+        a.compounds
+            .keys()
+            .filter(|k| b.compounds.contains_key(*k))
+            .all(|k| {
+                let av = a.compounds.get(k).unwrap();
+                let bv = b.compounds.get(k).unwrap();
+                if *k == "cats" || *k == "trees" {
+                    bv > av
+                } else if *k == "pomeranians" || *k == "goldfish" {
+                    bv < av
+                } else {
+                    av == bv
+                }
+            })
+    }
+}
+
+struct Problem<'a> {
+    sues: Box<[Sue<'a>]>,
+}
+impl<'a> Problem<'a> {
+    fn from_str(s: &'a str) -> AocResult<Self> {
+        Ok(Problem {
+            sues: Sue::gather(s.lines())?.into_boxed_slice(),
+        })
+    }
+
+    fn matches<P: Part>(&self) -> impl Iterator<Item = &Sue<'_>> {
+        let output = Sue::new(hashmap! {
+            "children" => 3,
+        "cats" => 7,
+        "samoyeds" => 2,
+        "pomeranians" => 3,
+        "akitas" => 0,
+        "vizslas" => 0,
+        "goldfish" => 5,
+        "trees" => 3,
+        "cars" => 2,
+        "perfumes" => 1,
+        });
+
+        self.sues.iter().filter(move |s| P::matches(&output, s))
+    }
+}
+
+fn solve<P: Part>(input: &str) -> AocResult<Answer> {
+    // Generation
+    let problem = Problem::from_str(input)?;
+
+    // Show matches
+    /*for sue in problem.matches::<PartA>() {
+        println!("{:?}", sue);
+    }*/
+
+    // Process
+    let sue = problem.matches::<P>().next().unwrap();
+    Ok(Answer::Unsigned(sue.number.into()))
 }
 
 pub const SOLUTION: Solution = Solution {
     day: 16,
     name: "Aunt Sue",
-    solvers: &[
-        // Part a)
-        |input| {
-            // Generation
-
-            // Process
-            Ok(0u64.into())
-        },
-    ],
+    solvers: &[solve::<PartA>, solve::<PartB>],
 };
