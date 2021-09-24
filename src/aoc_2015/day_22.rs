f814e219caf3a5c7909864104f56dd3039549c32
Dan Whitman <dwhitman44@gmail.com>
2021-09-24 17:01:28 -0400

    Completed the 2015 day 22 problem!

--- src/aoc_2015/day_22.rs
+++ src/aoc_2015/day_22.rs
@@ -1,4 +1,7 @@
 use nom::{combinator::map, sequence::tuple};
+use std::fmt;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 use crate::aoc::{parse::field_line_parser, prelude::*};
 
@@ -9,19 +12,21 @@ mod tests {
     use Answer::Unsigned;
 
     solution_test! {
-    vec![],
-    "Hit Points: 58
+    vec![Unsigned(1269), Unsigned(1309)],
+    "Hit Points: 40
     Damage: 9",
-    vec![1u64].answer_vec()
+    vec![734u64, 754].answer_vec()
     }
 }
 
-#[derive(new, Debug)]
+#[derive(new, Clone, Debug)]
 struct Character {
     hit_points: u32,
     damage: u32,
     mana: u32,
     armor: u32,
+    #[new(value = "Vec::new()")]
+    spells: Vec<Box<dyn Spell>>,
 }
 impl Character {
     fn heal(&mut self, amount: u32) {
@@ -34,6 +39,46 @@ impl Character {
 
     fn dead(&self) -> bool {
         self.hit_points == 0
+    }
+
+    fn turn_cast(&mut self, spell_type: SpellType, opponent: &mut Character) -> bool {
+        let spell = spell_type.create();
+
+        // Apply effects
+        self.apply_effects(opponent);
+        opponent.apply_effects(self);
+
+        // Check if this can be cast
+        if self.dead() || spell.cost() > self.mana || self.spells.contains(&spell) {
+            return false;
+        }
+
+        // Cast spell
+        //println!("Casting {}!", spell.name());
+        self.mana -= spell.cost();
+        self.spells.push(spell);
+        true
+    }
+
+    fn turn_attack(&mut self, opponent: &mut Character) {
+        // Apply effects
+        self.apply_effects(opponent);
+        opponent.apply_effects(self);
+
+        if !self.dead() {
+            //println!("Attacks!");
+            opponent.hurt(self.damage.saturating_sub(opponent.armor).max(1));
+        }
+    }
+
+    fn apply_effects(&mut self, opponent: &mut Character) {
+        let mut spells: Vec<Box<dyn Spell>> = self.spells.drain(..).collect();
+        for spell in spells.iter_mut() {
+            //spell.borrow_mut().apply_effect(self, opponent)
+            spell.apply_effect(self, opponent)
+        }
+        self.spells
+            .extend(spells.into_iter().filter(|s| !s.expired()))
     }
 }
 impl Parseable<'_> for Character {
@@ -51,28 +96,49 @@ impl Parseable<'_> for Character {
     }
 }
 
-trait Spell {
-    fn new() -> Self;
+trait Spell: SpellClone {
+    fn new() -> Self
+    where
+        Self: Sized;
     fn name(&self) -> &'static str;
     fn cost(&self) -> u32;
-    fn turn(&mut self, player: &mut Character, opponent: &mut Character);
+    fn apply_effect(&mut self, player: &mut Character, opponent: &mut Character);
     fn expired(&self) -> bool;
 }
-macro_rules! spell_eq {
-    ($type: ident) => {
-        impl PartialEq for $type {
-            fn eq(&self, other: &Self) -> bool {
-                self.name() == other.name()
-            }
-        }
-        impl Eq for $type {}
-    };
+impl PartialEq for dyn Spell {
+    fn eq(&self, other: &Self) -> bool {
+        self.name() == other.name()
+    }
+}
+impl fmt::Debug for dyn Spell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.name())
+    }
 }
 
+// This is a workaround that allows these trait objects to be cloned.
+// See: https://stackoverflow.com/questions/30353462/how-to-clone-a-struct-storing-a-boxed-trait-object
+trait SpellClone {
+    fn clone_box(&self) -> Box<dyn Spell>;
+}
+impl<T> SpellClone for T
+where
+    T: 'static + Clone + Spell,
+{
+    fn clone_box(&self) -> Box<dyn Spell> {
+        Box::new(self.clone())
+    }
+}
+impl Clone for Box<dyn Spell> {
+    fn clone(&self) -> Self {
+        self.clone_box()
+    }
+}
+
+#[derive(Clone)]
 struct MagicMissile {
     used: bool,
 }
-spell_eq! {MagicMissile}
 impl Spell for MagicMissile {
     fn new() -> Self {
         MagicMissile { used: false }
@@ -86,7 +152,7 @@ impl Spell for MagicMissile {
         53
     }
 
-    fn turn(&mut self, _player: &mut Character, opponent: &mut Character) {
+    fn apply_effect(&mut self, _player: &mut Character, opponent: &mut Character) {
         if !self.used {
             opponent.hurt(4);
         }
@@ -98,10 +164,10 @@ impl Spell for MagicMissile {
     }
 }
 
+#[derive(Clone)]
 struct Drain {
     used: bool,
 }
-spell_eq! {Drain}
 impl Spell for Drain {
     fn new() -> Self {
         Drain { used: false }
@@ -115,7 +181,7 @@ impl Spell for Drain {
         73
     }
 
-    fn turn(&mut self, player: &mut Character, opponent: &mut Character) {
+    fn apply_effect(&mut self, player: &mut Character, opponent: &mut Character) {
         if !self.used {
             opponent.hurt(2);
             player.heal(2);
@@ -128,10 +194,10 @@ impl Spell for Drain {
     }
 }
 
+#[derive(Clone)]
 struct Shield {
     turns: u8,
 }
-spell_eq! {Shield}
 impl Spell for Shield {
     fn new() -> Self {
         Shield { turns: 0 }
@@ -145,7 +211,7 @@ impl Spell for Shield {
         113
     }
 
-    fn turn(&mut self, player: &mut Character, opponent: &mut Character) {
+    fn apply_effect(&mut self, player: &mut Character, _opponent: &mut Character) {
         if self.turns == 0 {
             player.armor += 7;
         }
@@ -160,10 +226,10 @@ impl Spell for Shield {
     }
 }
 
+#[derive(Clone)]
 struct Poison {
     turns: u8,
 }
-spell_eq! {Poison}
 impl Spell for Poison {
     fn new() -> Self {
         Poison { turns: 0 }
@@ -177,7 +243,7 @@ impl Spell for Poison {
         173
     }
 
-    fn turn(&mut self, player: &mut Character, opponent: &mut Character) {
+    fn apply_effect(&mut self, _player: &mut Character, opponent: &mut Character) {
         if !self.expired() {
             opponent.hurt(3);
         }
@@ -189,10 +255,10 @@ impl Spell for Poison {
     }
 }
 
+#[derive(Clone)]
 struct Recharge {
     turn: u8,
 }
-spell_eq! {Recharge}
 impl Spell for Recharge {
     fn new() -> Self {
         Recharge { turn: 0 }
@@ -206,7 +272,7 @@ impl Spell for Recharge {
         229
     }
 
-    fn turn(&mut self, player: &mut Character, opponent: &mut Character) {
+    fn apply_effect(&mut self, player: &mut Character, _opponent: &mut Character) {
         if !self.expired() {
             player.mana += 101;
         }
@@ -218,6 +284,80 @@ impl Spell for Recharge {
     }
 }
 
+#[derive(Clone, Copy, EnumIter)]
+enum SpellType {
+    MagicMissile,
+    Drain,
+    Shield,
+    Poison,
+    Recharge,
+}
+impl SpellType {
+    fn create(&self) -> Box<dyn Spell> {
+        match *self {
+            SpellType::MagicMissile => Box::new(MagicMissile::new()),
+            SpellType::Drain => Box::new(Drain::new()),
+            SpellType::Shield => Box::new(Shield::new()),
+            SpellType::Poison => Box::new(Poison::new()),
+            SpellType::Recharge => Box::new(Recharge::new()),
+        }
+    }
+}
+
+fn solve(player: Character, boss: Character, hard_mode: bool) -> AocResult<u64> {
+    fn solve_rec(
+        level: usize,
+        spent: u32,
+        min_spent: &mut Option<u32>,
+        player: Character,
+        boss: Character,
+        hard_mode: bool,
+    ) {
+        let _indent = " ".repeat(level);
+
+        // Create play branch with every spell cast on the player's turn
+        for spell_type in SpellType::iter() {
+            let mut player = player.clone();
+            let mut boss = boss.clone();
+            if hard_mode {
+                player.hurt(1)
+            }
+            if !player.dead() && player.turn_cast(spell_type, &mut boss) {
+                let spell = spell_type.create();
+                let spent = spent + spell.cost();
+                //println!("{}Casted {}", _indent, spell.name());
+                // Abandon branch if the player has already spent too much
+                if let Some(s) = min_spent {
+                    if spent > *s {
+                        //println!("{}Abandoning branch!", _indent);
+                        continue;
+                    }
+                }
+
+                boss.turn_attack(&mut player);
+
+                if boss.dead() {
+                    //println!("{}Boss killed with {} mana spent!", _indent, spent);
+                    *min_spent = match min_spent {
+                        None => Some(spent),
+                        Some(s) => Some(spent.min(*s)),
+                    }
+                } else if player.dead() {
+                    //println!("{}Player was killed!", _indent);
+                } else {
+                    solve_rec(level + 1, spent, min_spent, player, boss, hard_mode);
+                }
+            }
+        }
+    }
+
+    let mut min_spent = None;
+    solve_rec(0, 0, &mut min_spent, player, boss, hard_mode);
+    Ok(min_spent
+        .ok_or_else(|| AocError::Process("The boss always wins!".into()))?
+        .into())
+}
+
 pub const SOLUTION: Solution = Solution {
     day: 22,
     name: "Wizard Simulator 20XX",
@@ -226,11 +366,35 @@ pub const SOLUTION: Solution = Solution {
         |input| {
             // Generation
             let boss = Character::from_str(input)?;
+            let player = Character::new(50, 0, 500, 0);
 
-            println!("TODO: {:?}", boss);
+            // Test for example battle
+            /*let mut player = Character::new(10, 0, 250, 0);
+            let mut boss = Character::new(14, 8, 0, 0);
+            use SpellType::*;
+            for spell in [Recharge, Shield, Drain, Poison, MagicMissile] {
+                println!("\nPlayer turn:");
+                println!("Player: {:?}", player);
+                println!("Boss: {:?}", boss);
+                player.turn_cast(spell, &mut boss);
+
+                println!("\nBoss turn:");
+                println!("Player: {:?}", player);
+                println!("Boss: {:?}", boss);
+                boss.turn_attack(&mut player);
+            }*/
 
             // Process
-            Ok(0u64.into())
+            Ok(solve(player, boss, false)?.into())
+        },
+        // Part b)
+        |input| {
+            // Generation
+            let boss = Character::from_str(input)?;
+            let player = Character::new(50, 0, 500, 0);
+
+            // Process
+            Ok(solve(player, boss, true)?.into())
         },
     ],
 };
