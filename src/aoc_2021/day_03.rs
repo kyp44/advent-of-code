d070097b579e47f6f2d936aa4ee55a36c2d3e076
Dan Whitman <dwhitman44@gmail.com>
2022-02-16 16:14:55 -0500

    Completed the 2021 day 3 problem.

--- src/aoc_2021/day_03.rs
+++ src/aoc_2021/day_03.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{cmp::Ordering, ops::Not, str::FromStr};
 
 use itertools::Itertools;
 use nom::{character::complete::one_of, combinator::map, multi::many1};
@@ -12,7 +12,7 @@ mod tests {
     use Answer::Unsigned;
 
     solution_test! {
-    vec![Unsigned(112312)],
+    vec![Unsigned(3320834), Unsigned(4481199)],
     "00100
 11110
 10110
@@ -25,41 +25,58 @@ mod tests {
 11001
 00010
 01010",
-    vec![198u64].answer_vec()
+    vec![198u64, 230].answer_vec()
     }
 }
 
+#[derive(Clone)]
 struct ReportLine {
     bit_vec: Vec<bool>,
 }
-
+impl ReportLine {
+    fn value(&self) -> u64 {
+        self.bit_vec.iter().fold(0, |a, b| 2 * a + u64::from(*b))
+    }
+}
 impl Parseable<'_> for ReportLine {
     fn parser(input: &str) -> NomParseResult<Self> {
         map(many1(one_of("01")), |v| Self {
-            bit_vec: v
-                .into_iter()
-                .map(|b| if b == '1' { true } else { false })
-                .collect(),
+            bit_vec: v.into_iter().map(|b| b == '1').collect(),
         })(input)
     }
 }
-impl Into<u64> for ReportLine {
-    fn into(self) -> u64 {
-        self.bit_vec
-            .into_iter()
-            .fold(0, |a, b| 2 * a + u64::from(b))
+impl FromIterator<bool> for ReportLine {
+    fn from_iter<T: IntoIterator<Item = bool>>(iter: T) -> Self {
+        Self {
+            bit_vec: iter.into_iter().collect(),
+        }
+    }
+}
+impl Not for ReportLine {
+    type Output = ReportLine;
+
+    fn not(mut self) -> Self::Output {
+        self.bit_vec.iter_mut().for_each(|b| *b = !*b);
+        self
     }
 }
 
+#[derive(Clone)]
 struct Report {
-    lines: Box<[ReportLine]>,
+    lines: Vec<ReportLine>,
     size: usize,
 }
+enum MostCommon {
+    Ones,
+    Zeros,
+    Equal,
+}
+
 impl FromStr for Report {
     type Err = AocError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let lines = ReportLine::gather(s.lines())?.into_boxed_slice();
+        let lines = ReportLine::gather(s.lines())?;
         let size = lines[0].bit_vec.len();
 
         if !lines.iter().map(|l| l.bit_vec.len()).all_equal() {
@@ -72,20 +89,54 @@ impl FromStr for Report {
     }
 }
 impl Report {
+    fn most_common(&self, bit_num: usize) -> MostCommon {
+        let n_ones: usize = self.lines.iter().filter_count(|l| l.bit_vec[bit_num]);
+        match (2 * n_ones).cmp(&self.lines.len()) {
+            Ordering::Less => MostCommon::Zeros,
+            Ordering::Equal => MostCommon::Equal,
+            Ordering::Greater => MostCommon::Ones,
+        }
+    }
+
+    fn filter_bit(&mut self, bit_num: usize, bit_value: bool) {
+        self.lines.retain(|l| l.bit_vec[bit_num] == bit_value)
+    }
+
     fn power_consumption(&self) -> AocResult<u64> {
-        let gamma = (0..self.size)
-            .map(|n| {
-                let n_ones: usize = self.lines.iter().filter_count(|l| l.bit_vec[n]);
-                // 2*x > n  <=>  x > n - x
-                2 * n_ones > self.lines.len()
-            })
+        let gamma: ReportLine = (0..self.size)
+            .map(|n| matches!(self.most_common(n), MostCommon::Ones))
             .collect();
-        let epsilon = !gamma.clone();
-        let x: u64 = &gamma.into();
+        let gamma_val = gamma.value();
+        let epsilon_val = (!gamma).value();
 
-        println!("gamma: {}, epsilon: {}", gamma, epsilon);
+        Ok(gamma_val * epsilon_val)
+    }
 
-        Ok(0)
+    fn calculate_rating<F: Fn(MostCommon) -> bool>(&self, criteria: F) -> AocResult<u64> {
+        let mut report = self.clone();
+
+        for n in 0..self.size {
+            if report.lines.len() == 1 {
+                break;
+            }
+
+            report.filter_bit(n, criteria(report.most_common(n)))
+        }
+
+        if report.lines.len() == 1 {
+            Ok(report.lines[0].value())
+        } else {
+            Err(AocError::Process(
+                "Rating did not filter report lines down to only one".into(),
+            ))
+        }
+    }
+
+    fn life_support_rating(&self) -> AocResult<u64> {
+        let oxygen_rating = self.calculate_rating(|com| !matches!(com, MostCommon::Zeros))?;
+        let co2_rating = self.calculate_rating(|com| matches!(com, MostCommon::Zeros))?;
+
+        Ok(oxygen_rating * co2_rating)
     }
 }
 
@@ -100,6 +151,14 @@ pub const SOLUTION: Solution = Solution {
 
             // Process
             Ok(report.power_consumption()?.into())
+        },
+        // Part b)
+        |input| {
+            // Generation
+            let report = Report::from_str(input)?;
+
+            // Process
+            Ok(report.life_support_rating()?.into())
         },
     ],
 };
