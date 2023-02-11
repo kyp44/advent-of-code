2ff9523be2722857ae2dbe72b0f65fa7d02f299c
Dan Whitman <dwhitman44@mgail.com>
2023-02-11 14:46:51 -0500

    Refactored the 2020 day 13 problem to use `bare_metal_modulo` instead of manually implementing the Chinese Remainder Theorem. May still have more refactoring to use `bare_metal_modulo` for other problems though.

--- src/aoc_2020/day_13.rs
+++ src/aoc_2020/day_13.rs
@@ -29,6 +29,7 @@ mod tests {
 /// Contains solution implementation items.
 mod solution {
     use super::*;
+    use bare_metal_modulo::{MNum, ModNum};
     use derive_new::new;
     use itertools::Itertools;
     use nom::{
@@ -56,7 +57,7 @@ mod solution {
         /// The earliest time we can depart.
         earliest_time: u64,
         /// List of bus IDs (`Some`), including buses that are not running (`None`).
-        bus_ids: Vec<Option<u64>>,
+        bus_ids: Vec<Option<ModNum<u64>>>,
     }
     impl Parseable<'_> for Schedule {
         fn parser(input: &str) -> NomParseResult<&str, Self> {
@@ -68,37 +69,38 @@ mod solution {
                 ),
                 |(earliest_time, vs): (u64, Vec<&str>)| Schedule {
                     earliest_time,
-                    bus_ids: vs.into_iter().map(|s| s.parse().ok()).collect(),
+                    bus_ids: vs
+                        .into_iter()
+                        .map(|s| s.parse().ok().map(|id| ModNum::new(0, id)))
+                        .collect(),
                 },
             )(input)
         }
     }
     impl Schedule {
         /// Returns an [`Iterator`] of all bus IDs, ignoring those that are not running.
-        fn valid_ids(&self) -> impl Iterator<Item = u64> + '_ {
+        fn valid_ids(&self) -> impl Iterator<Item = ModNum<u64>> + '_ {
             self.bus_ids.iter().filter_map(|id| *id)
         }
 
         /// Determines the earlier bus that we can take.
         pub fn earliest_bus(&self) -> EarliestBus {
-            let time_until = |id: u64| neg_modulo(self.earliest_time, id);
-            let bus_id = self
-                .valid_ids()
-                .min_by(|a, b| time_until(*a).cmp(&time_until(*b)))
-                .unwrap();
+            let time_until = |id| id - self.earliest_time;
+            let bus_id = self.valid_ids().min_by_key(|id| time_until(*id)).unwrap();
 
-            EarliestBus::new(bus_id, time_until(bus_id))
+            EarliestBus::new(bus_id.m(), time_until(bus_id).a())
         }
 
         /// Determines the earliest time at which buses depart in consecutive minutes, with gaps
         /// for non-running buses.
         pub fn earliest_consecutive_departures_time(&self) -> AocResult<u64> {
             // This problem is effectively the Chinese Remainder Theorem to solve a system
-            // of modulo congruences. These can be solved so long as the modulo factors
-            // (in our case the set of bus IDs) are all pairwise co-prime. So first we check
-            // that this is the case to guarantee that there will be a solution.
+            // of modulo congruences.
+            // These can be solved so long as the modulo factors (in our case the set of bus
+            // IDs) are all pairwise co-prime. So first we check that this is the case to
+            // guarantee that there will be a solution.
             for v in self.valid_ids().combinations(2) {
-                if gcd(v[0], v[1]) > 1 {
+                if gcd(v[0].m(), v[1].m()) > 1 {
                     return Err(AocError::Process(
                         format!(
                             "Part two may not be solvable because {} and {} are not co-prime",
@@ -108,70 +110,21 @@ mod solution {
                     ));
                 }
             }
-            // First get an iterator of tuples of (a, m), where a is congruence (time
-            // between timestamp and bus leaving) and m is the modulo value (bus ID)
-            // for each bus and ordered in descending order by m, which results in
-            // the fastest solution.
-            let mut conditions = self
-                .bus_ids
-                .iter()
-                .enumerate()
-                .filter_map(|(i, ido)| -> Option<(u64, u64)> {
-                    ido.map(|id| (neg_modulo(i.try_into().unwrap(), id), id))
-                })
-                .sorted_by(|t1, t2| t1.1.cmp(&t2.1).reverse());
 
-            // Now we use a sieve search as described at
-            // https://en.wikipedia.org/wiki/Chinese_remainder_theorem#Search_by_sieving
-            let (mut t, mut m) = conditions.next().unwrap();
-            for (na, nm) in conditions {
-                for x in ModuloValues::new(t, m) {
-                    if (x % nm) == na {
-                        // Found a solution that meets all conditions so far
-                        t = x;
-                        m *= nm;
-                        break;
-                    }
-                }
-            }
+            // The solution will be the earliest timestamp.
+            // So first form an iterator of modulo numbers, to all of which the solution
+            // must be congruent.
+            let mod_iter = self.bus_ids.iter().enumerate().filter_map(|(i, id)| {
+                id.map(|m| -ModNum::<i128>::new(i.try_into().unwrap(), m.m().into()))
+            });
 
-            Ok(t)
-        }
-    }
-
-    /// Returns `-d` modulo `m`.
-    ///
-    /// Note that is correct and differs from `m - (d % m)` when `d == 0`.
-    fn neg_modulo(d: u64, m: u64) -> u64 {
-        let md: i64 = -TryInto::<i64>::try_into(d).unwrap();
-        let m: i64 = m.try_into().unwrap();
-        (md.rem_euclid(m)).try_into().unwrap()
-    }
-
-    /// Endless [`Iterator`] over successive numbers that are all the same modulo some other number.
-    struct ModuloValues {
-        /// The next value in the sequence.
-        current: u64,
-        /// Modulo number.
-        modulo: u64,
-    }
-    impl ModuloValues {
-        /// Create a new [`Iterator`], starting at the lowest positive number and
-        /// going through all numbers congruent to `a` modulo `modulo`.
-        fn new(a: u64, modulo: u64) -> ModuloValues {
-            ModuloValues {
-                current: a % modulo,
-                modulo,
-            }
-        }
-    }
-    impl Iterator for ModuloValues {
-        type Item = u64;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            let r = Some(self.current);
-            self.current += self.modulo;
-            r
+            // Solve the congruence system.
+            // Note: This requires signed numeric types for some reason, which is annoying.
+            Ok(ModNum::chinese_remainder_system(mod_iter)
+                .unwrap()
+                .a()
+                .try_into()
+                .unwrap())
         }
     }
 }
