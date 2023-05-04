cc67fef01944121d88f2fb2255efa253e5312310
Dan Whitman <daniel.whitman.2@us.af.mil>
2023-05-04 07:05:53 -0400

    Transfer. Troubleshooting the best metric tree search code.

--- src/aoc/tree_search.rs
+++ src/aoc/tree_search.rs
@@ -3,14 +3,11 @@
 //! TODO
 
 use derive_new::new;
-use num::PrimInt;
+use itertools::Itertools;
 
-use std::{collections::HashMap, hash::Hash, ops::Add};
+use std::{cell::RefCell, collections::HashMap, ops::Add, rc::Rc};
 
-// TODO: Ideas for versatility (Metric, Count, First solution, etc.):
-// Some kind of general state trait that we implement for each
-// Different overall TreeSearch traits for each of these
-// How do we handle special case, e.g. counting wins for each player?
+// TODO: Can we make the general stuff private if we can cover all use cases with more specific implementations?
 
 #[derive(new)]
 pub struct Child<N: TreeNode> {
@@ -18,30 +15,39 @@ pub struct Child<N: TreeNode> {
     state: N::DownwardState,
 }
 
+trait TreeUpwardState<N: TreeNode>: Sized {
+    fn new(current: &Child<N>) -> Self;
+
+    fn stop(&self, current: &Child<N>) -> Option<Self>;
+
+    fn incorporate_child(&mut self, current: &Child<N>, child_state: Self);
+
+    fn finalize(&mut self, current: Child<N>);
+}
+
 pub trait TreeNode: Sized {
     type DownwardState: Default;
-    type GlobalState: Default;
+    type UpwardState: TreeUpwardState<Self>;
 
-    fn node_children(
-        &self,
-        downward_state: &Self::DownwardState,
-        global_state: &mut Self::GlobalState,
-    ) -> Vec<Child<Self>>;
+    fn node_children(&self, downward_state: &Self::DownwardState) -> Vec<Child<Self>>;
 
-    fn search_tree(self) -> Self::GlobalState {
-        fn rec<N: TreeNode>(current: Child<N>, global_state: &mut N::GlobalState) {
-            // Recurse for each leaf
-            for child in current.node.node_children(&current.state, global_state) {
-                rec(child, global_state);
+    fn search_tree(self) -> Self::UpwardState {
+        fn rec<N: TreeNode>(current: Child<N>) -> N::UpwardState {
+            let mut upward_state = N::UpwardState::new(&current);
+            match upward_state.stop(&current) {
+                Some(s) => s,
+                None => {
+                    // Recurse for each leaf
+                    for child in current.node.node_children(&current.state) {
+                        upward_state.incorporate_child(&current, rec(child));
+                    }
+                    upward_state.finalize(current);
+                    upward_state
+                }
             }
         }
 
-        let mut global_state = Self::GlobalState::default();
-        rec(
-            Child::new(self, Self::DownwardState::default()),
-            &mut global_state,
-        );
-        global_state
+        rec(Child::new(self, Self::DownwardState::default()))
     }
 }
 
@@ -63,20 +69,119 @@ pub struct MetricChild<N: BestMetricTreeNode> {
     node: N,
     cost: N::Metric,
 }
-pub struct MetricCost<M: Metric>(M);
-impl<M: Metric> Default for MetricCost<M> {
-    fn default() -> Self {
-        Self(M::INITIAL_COST)
-    }
+
+#[derive(Clone)]
+struct MetricGlobalState<N: BestMetricTreeNode> {
+    best_metric: N::Metric,
+    seen: HashMap<N, N::Metric>,
 }
-pub struct MetricBest<M: Metric>(M);
-impl<M: Metric> Default for MetricBest<M> {
+impl<N: BestMetricTreeNode> Default for MetricGlobalState<N> {
     fn default() -> Self {
-        Self(M::INITIAL_BEST)
+        Self {
+            best_metric: N::Metric::INITIAL_BEST,
+            seen: HashMap::new(),
+        }
     }
 }
 
-pub trait BestMetricTreeNode: Sized {
+pub struct MetricDownwardState<N: BestMetricTreeNode> {
+    global_state: Rc<RefCell<MetricGlobalState<N>>>,
+    cumulative_cost: N::Metric,
+    node_cost: N::Metric,
+    _level: usize,
+}
+impl<N: BestMetricTreeNode> Default for MetricDownwardState<N> {
+    fn default() -> Self {
+        Self {
+            global_state: Default::default(),
+            cumulative_cost: N::Metric::INITIAL_COST,
+            node_cost: N::Metric::INITIAL_COST,
+            _level: 0,
+        }
+    }
+}
+
+fn level_spaces(level: usize) -> String {
+    format!("{}{}:", (0..level).map(|_| "  ").join(""), level)
+}
+
+pub struct BestMetric<N: BestMetricTreeNode>(N::Metric);
+impl<N: BestMetricTreeNode> TreeUpwardState<N> for BestMetric<N> {
+    fn new(current: &Child<N>) -> Self {
+        println!(
+            "{} TODO About to process node with node cost {:?}",
+            level_spaces(current.state._level),
+            current.state.node_cost,
+        );
+        Self(N::Metric::INITIAL_BEST)
+    }
+
+    fn stop(&self, current: &Child<N>) -> Option<Self> {
+        let mut global_state = current.state.global_state.as_ref().borrow_mut();
+
+        // Are we at a terminal node?
+        if current.node.end_state() {
+            // Update global if better.
+            global_state
+                .best_metric
+                .update_if_better(current.state.cumulative_cost);
+
+            println!(
+                "{} TODO Terminus global: {:?} current cum: {:?}",
+                level_spaces(current.state._level),
+                global_state.best_metric,
+                current.state.cumulative_cost,
+            );
+            return Some(Self(N::Metric::INITIAL_COST));
+        }
+
+        // Is our cost already too high?
+        if global_state
+            .best_metric
+            .is_better(&current.state.cumulative_cost)
+        {
+            println!(
+                "{} TODO Cost too high global: {:?} current cum: {:?}",
+                level_spaces(current.state._level),
+                global_state.best_metric,
+                current.state.cumulative_cost,
+            );
+            return Some(Self(N::Metric::INITIAL_BEST));
+        }
+
+        // Have we seen this node already?
+        global_state.seen.get(&current.node).map(|bm| {
+            println!(
+                "{} TODO have seen node before global: {:?} current cum: {:?} seen: {:?} total: {:?}",
+                level_spaces(current.state._level),
+                global_state.best_metric,
+                current.state.cumulative_cost,
+                bm,
+                current.state.cumulative_cost + *bm,
+            );
+            Self(bm.clone())
+        })
+    }
+
+    fn incorporate_child(&mut self, current: &Child<N>, child: Self) {
+        self.0.update_if_better(child.0 + current.state.node_cost);
+        println!(
+            "{} TODO Incorporated child: best for child + this {:?} updated best: {:?}",
+            level_spaces(current.state._level),
+            child.0 + current.state.node_cost,
+            self.0
+        )
+    }
+
+    fn finalize(&mut self, current: Child<N>) {
+        let mut global_state = current.state.global_state.as_ref().borrow_mut();
+
+        // Mark this as seen
+        global_state.seen.insert(current.node, self.0.clone());
+    }
+}
+
+pub trait BestMetricTreeNode: Sized + Eq + std::hash::Hash {
     type Metric: Metric + std::fmt::Debug;
 
     fn end_state(&self) -> bool;
@@ -88,34 +193,32 @@ pub trait BestMetricTreeNode: Sized {
     }
 }
 impl<N: BestMetricTreeNode> TreeNode for N {
-    type DownwardState = MetricCost<N::Metric>;
-    type GlobalState = MetricBest<N::Metric>;
+    type DownwardState = MetricDownwardState<N>;
+    type UpwardState = BestMetric<N>;
 
-    fn node_children(
-        &self,
-        downward_state: &Self::DownwardState,
-        global_state: &mut Self::GlobalState,
-    ) -> Vec<Child<Self>> {
-        // Are we at an end node?
-        // TODO: What about if we've seen this state before?
-        if self.end_state() {
-            // We are at an end node so update global best if we beat it
-            global_state.0.update_if_better(downward_state.0);
-            //println!("TODO End node! {:?}", global_state.0);
-            vec![]
-        } else if global_state.0.is_better(&downward_state.0) {
-            // Our cost is already too high so just stop
-            /* println!(
-                "TODO Stopped early {:?} {:?}!",
-                global_state.0, downward_state.0,
-            ); */
-            vec![]
-        } else {
-            self.children(&downward_state.0)
-                .into_iter()
-                .map(|child| Child::new(child.node, MetricCost(downward_state.0 + child.cost)))
-                .collect()
-        }
+    fn node_children(&self, downward_state: &Self::DownwardState) -> Vec<Child<Self>> {
+        let x: Vec<Child<Self>> = self
+            .children(&downward_state.cumulative_cost)
+            .into_iter()
+            .map(|child| {
+                Child::new(
+                    child.node,
+                    MetricDownwardState {
+                        global_state: downward_state.global_state.clone(),
+                        cumulative_cost: downward_state.cumulative_cost + child.cost,
+                        node_cost: child.cost,
+                        _level: downward_state._level + 1,
+                    },
+                )
+            })
+            .collect();
+
+        println!(
+            "{} TODO have {} children.",
+            level_spaces(downward_state._level),
+            x.len()
+        );
+        x
     }
 }
 
