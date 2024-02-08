5bd73dbfee2031887f58caa993446567caed8b35
Dan Whitman <daniel.whitman.2@us.af.mil>
2024-02-08 12:50:31 -0500

    Transfer. Continues working on the 2022 day 16 problem.

--- src/aoc_2022/day_16.rs
+++ src/aoc_2022/day_16.rs
@@ -25,9 +25,10 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
 
 /// Contains solution implementation items.
 mod solution {
+    use super::*;
     use aoc::{
         parse::trim,
-        tree_search::{GlobalState, GlobalStateTreeNode},
+        tree_search::{GlobalAction, GlobalState, GlobalStateTreeNode},
     };
     use derive_new::new;
     use itertools::Itertools;
@@ -41,10 +42,10 @@ mod solution {
     use std::{
         collections::{HashMap, HashSet},
         marker::PhantomData,
-        ops::Add,
     };
 
-    use super::*;
+    const STARTING_VALVE: &str = "AA";
+    const MINUTES_ALLOWED: u8 = 30;
 
     #[derive(Debug)]
     struct Valve {
@@ -79,6 +80,8 @@ mod solution {
         }
     }
 
+    type ValveMap<'a> = HashMap<&'a str, CondensedValve<'a>>;
+
     #[derive(Debug)]
     pub struct Volcano {
         valves: Vec<Valve>,
@@ -93,11 +96,11 @@ mod solution {
         }
     }
     impl Volcano {
-        pub fn maximum_pressure_release(&self) -> u64 {
+        pub fn maximum_pressure_release(&self) -> AocResult<u64> {
             // TODO: Do the reduction up front if raw parsed Valves are not needed for part two.
 
             // First convert to the final form
-            let mut valves = self
+            let mut valves: ValveMap<'_> = self
                 .valves
                 .iter()
                 .map(|v| {
@@ -110,7 +113,24 @@ mod solution {
                         },
                     )
                 })
-                .collect::<HashMap<_, _>>();
+                .collect();
+
+            // Extract the initial tunnel choices from valve AA
+            // TODO: Change to InvalidInput if moved.
+            let initial_tunnels = valves
+                .get(STARTING_VALVE)
+                .ok_or(AocError::Process(
+                    format!("Valve {STARTING_VALVE} does not exist!").into(),
+                ))
+                .and_then(|aa| {
+                    if aa.flow_rate > 0 {
+                        Err(AocError::Process(
+                            format!("The valve {STARTING_VALVE} flow rate is nonzero!").into(),
+                        ))
+                    } else {
+                        Ok(aa.tunnels.iter().cloned().collect::<HashSet<_>>())
+                    }
+                })?;
 
             // Collect those valves with zero flow rate
             let zero_flow_rates = valves
@@ -124,7 +144,7 @@ mod solution {
                 })
                 .collect_vec();
 
-            // Now remove those valves with zero flow rate and substitute tunnels leading there with bypassed tunnels
+            // Now remove all valves with zero flow rate and substitute tunnels leading there with bypassed tunnels
             for zfr in zero_flow_rates {
                 let zfr_tunnels = valves.remove(zfr).unwrap().tunnels;
                 let zfr = Tunnel::from(zfr);
@@ -154,42 +174,60 @@ mod solution {
                 }
             }
 
-            // TODO: Need to deal with the fact that AA, the starting valve, has ZFR, so is currently removed here!
+            // Now, re-add the starting valve which, once left, will never be returned to
+            valves.insert(
+                STARTING_VALVE,
+                CondensedValve {
+                    label: STARTING_VALVE,
+                    flow_rate: 0,
+                    tunnels: initial_tunnels,
+                },
+            );
 
             println!("TODO: {valves:?}");
 
-            // First reduce the valve network to remove valves with no flow rate.
-
-            // TODO: delete
-            /* let no_flow_rate_valves = self
-                .valves
-                .iter()
-                .filter_map(|v| {
-                    if v.flow_rate == 0 {
-                        Some((v.label.as_str(), v))
-                    } else {
-                        None
-                    }
-                })
-                .collect::<HashMap<_, _>>();
-
-            fn rec_build_tunnels(valve: &Valve, )
-
-            let condensed_valves = HashMap::new();
-            for valve in self.valves.iter().filter(|v| v.flow_rate > 0) {
-
-                let tunnels = valve
-                    .tunnels
-                    .iter()
-                    .map(|t| Tunnel::from(t))
-                    .collect_vec();
-            } */
-
-            0
+            Ok(0)
         }
     }
 
-    #[derive(Debug, Eq, new)]
+    #[derive(Debug, Clone)]
+    struct OpenedValves {
+        cumulative_released: u64,
+        total_flow_per_minute: u64,
+        time_passed: u8,
+    }
+    impl Default for OpenedValves {
+        fn default() -> Self {
+            Self {
+                cumulative_released: 0,
+                total_flow_per_minute: 0,
+                time_passed: 0,
+            }
+        }
+    }
+    impl OpenedValves {
+        pub fn open_valve(&mut self, flow_rate: u8) {
+            self.total_flow_per_minute += u64::from(flow_rate);
+        }
+
+        pub fn advance_time(&mut self, minutes: u8) {
+            let time = minutes.min(MINUTES_ALLOWED - self.time_passed);
+            self.cumulative_released += self.total_flow_per_minute * u64::from(time);
+            self.time_passed += time;
+        }
+
+        pub fn run_out_clock(&mut self) -> u64 {
+            self.advance_time(MINUTES_ALLOWED);
+            self.cumulative_released
+        }
+
+        // None if time is not up
+        pub fn is_time_up(&self) -> Option<u64> {
+            (self.time_passed >= MINUTES_ALLOWED).then_some(self.cumulative_released)
+        }
+    }
+
+    #[derive(Debug, Eq, Clone, new)]
     struct Tunnel<'a> {
         to: &'a str,
         time: u8,
@@ -220,8 +258,16 @@ mod solution {
 
     #[derive(Debug)]
     struct SearchState<'a> {
-        valves: HashMap<&'a str, &'a Valve>,
-        closed_valves: HashSet<&'a str>,
+        valves: &'a ValveMap<'a>,
+        best_cumulative_released: u64,
+    }
+    impl<'a> SearchState<'a> {
+        pub fn new(valves: &'a ValveMap<'a>) -> Self {
+            Self {
+                valves,
+                best_cumulative_released: 0,
+            }
+        }
     }
     impl<'a> GlobalState<ValveNode<'a>> for SearchState<'a> {
         fn update_with_node(&mut self, node: &ValveNode<'a>) {
@@ -229,22 +275,33 @@ mod solution {
         }
 
         fn complete(&self) -> bool {
-            todo!()
+            false
         }
     }
 
     #[derive(Debug)]
     struct ValveNode<'a> {
-        _dummy: PhantomData<&'a ()>,
+        current: &'a CondensedValve<'a>,
+        closed_valves: HashSet<&'a str>,
+        opened_valves: OpenedValves,
+    }
+    impl<'a> ValveNode<'a> {
+        pub fn initial(search_state: &'a SearchState<'a>) -> Self {
+            Self {
+                current: search_state.valves.get(STARTING_VALVE).unwrap(),
+                closed_valves: search_state.valves.keys().copied().collect(),
+                opened_valves: OpenedValves::default(),
+            }
+        }
     }
 
     impl<'a> GlobalStateTreeNode for ValveNode<'a> {
         type GlobalState = SearchState<'a>;
 
-        fn recurse_action(
-            &self,
-            state: &Self::GlobalState,
-        ) -> aoc::tree_search::GlobalAction<Self> {
+        fn recurse_action(&self, state: &Self::GlobalState) -> GlobalAction<Self> {
+            if self.closed_valves.is_empty() {
+                // All valves are opened so just run out the clock
+            }
             todo!()
         }
     }
@@ -263,7 +320,7 @@ pub const SOLUTION: Solution = Solution {
             // Generation
             let volcano = Volcano::from_str(input.expect_input()?)?;
 
-            println!("TODO: {}", volcano.maximum_pressure_release());
+            println!("TODO: {}", volcano.maximum_pressure_release()?);
 
             // Process
             Ok(0u64.into())
